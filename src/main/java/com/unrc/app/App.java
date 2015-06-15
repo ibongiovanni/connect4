762e0bdaeb6b541f9856045fb3ef6dabf6bdead0
lukax77 <lucasducculi@gmail.com>
2015-06-15 19:54:18 -0300

    Graphic design changes

--- src/main/java/com/unrc/app/App.java
+++ src/main/java/com/unrc/app/App.java
@@ -6,20 +6,32 @@ import static spark.Spark.*;
 import spark.ModelAndView;
 import java.util.*;
 /**
- * Hello world!
+ * 
  *
  */
 public class App
 {
-    /**
-     *
-     */
-    public static void baseOpen(){
-        Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_development", "root", "root");
-    }
-
     public static void main( String[] args )
     {
+        staticFileLocation("/public");
+
+
+        /**
+         * Before-filters are evaluated before each request.
+         * Class Base will open a new connection and attach it to the current thread.
+         */
+        before((request, response) -> {
+            Base.open("com.mysql.jdbc.Driver", "jdbc:mysql://localhost/connect4_development", "root", "root");    
+        });
+
+        /**
+         * After-filters are evaluated after each request.
+         * Connection is closed and cleared from thread.
+         */
+        after((request, response) -> {
+            Base.close();    
+        });
+
         // Site style.
         get("/siteStyle", (req, res) -> {
             return new ModelAndView(null, "siteStyle.css");
@@ -31,16 +43,12 @@ public class App
        
         get("/users", (req, res) -> {       
 
-            baseOpen();           
-
             Map<String, Object> attributes = new HashMap();
 
             List<Rank> ranks = Rank.findBySQL("SELECT ranks.* FROM ranks JOIN users ON ranks.user_id = users.id ORDER BY number DESC");
             List<User> users = User.findBySQL("SELECT users.* FROM ranks JOIN users ON ranks.user_id = users.id ORDER BY number DESC");
             int size = ranks.size();// This way, you will pre-populate your list.
             int size2= users.size();
-
-            Base.close();
 
             attributes.put("ranks", ranks);
             attributes.put("users", users);
@@ -59,27 +67,39 @@ public class App
             user_data[1] = req.queryParams("last_name");
             user_data[2] = req.queryParams("email");
             
-            baseOpen();
-            
             User u = new User();
             u.set("first_name",user_data[0],"last_name",user_data[1],"email",user_data[2]);
             u.saveIt();
             u.newRank();
-            
-            Base.close();
-
+           
             return new ModelAndView(null, "home.mustache");
         }, new MustacheTemplateEngine());
 
         get("/play", (req,res) -> {
-            return new ModelAndView(null, "play.mustache");
+
+            Map<String, Object> attributes = new HashMap();
+
+            List<User> users = User.findAll();
+            int size = users.size();// This way, you will pre-populate your list.
+
+            List<HashMap> order = new LinkedList<HashMap>();
+            HashMap mp;
+            for (User u : users ) {
+                mp = new HashMap();
+                mp.put("id",u.getString("id"));
+                mp.put("itemName",u.getString("email"));
+                order.add(mp);
+            }
+
+            Map<String, Object> map = new HashMap();
+            map.put("order", order);
+
+            return new ModelAndView(map, "play.mustache");
         }, new MustacheTemplateEngine());
 
         post("/create_game", (req, res) -> {
             int id_player_1 = Integer.parseInt(req.queryParams("player_1"));
             int id_player_2 = Integer.parseInt(req.queryParams("player_2"));
-
-            baseOpen();
 
             Game g = new Game(id_player_1,id_player_2,6,7);
             g.saveIt();
@@ -91,13 +111,13 @@ public class App
             User u = User.findById(id_player_1);
             String name_player1 = u.getString("first_name");
             
-            Base.close();
-
             String message = name_player1 + " plays";
+            String color = "blue";
 
             Map map = new HashMap();
             map.put("game_id", game_id);
             map.put("message", message);
+            map.put("colored",color);
 
             int k = 0;
             for (int i = 0; i < height ; i++) {
@@ -116,8 +136,8 @@ public class App
             int game_id = Integer.parseInt(req.queryParams("game_id"));
             int column = Integer.parseInt(req.queryParams("column"));
             String message = "";
-
-            baseOpen();
+            String color = "";
+            boolean finished = false;
 
             Game g = Game.findById(game_id);
             int height = g.getInteger("height");
@@ -151,18 +171,22 @@ public class App
                     p.saveIt();
 
                     if (!grid.checkWin()) {
-                        if (ord % 2 != 0) { message = name_player2 + " plays"; }
-                        else { message = name_player1 + " plays"; }
+                        if (ord % 2 != 0) { message = name_player2 + " plays"; color = "red"; }
+                        else { message = name_player1 + " plays"; color = "blue"; }
                     }
                     else {
                         if (ord % 2 != 0) { 
                             message = name_player1 + " won the game!";
+                            color = "blue";
+                            finished = true;
                             g.set("winner", id_player_1);
                             g.saveIt();
                             u.updateRank(3);
                         }
                         else {
                             message = name_player2 + " won the game!";
+                            color = "red";
+                            finished = true;
                             g.set("winner", id_player_2);
                             g.saveIt();
                             v.updateRank(3);
@@ -171,22 +195,29 @@ public class App
                 }
                 else {
                     message = "ERROR: The column is full choose another!";
+                    color = "green";
                 }
             }
             else {
                 if (ord > maxPlays) { 
                 message = "The game was a tie !!!";
+                color = "yellow";
+                finished = true;
+                g.set("winner", 0);
+                g.saveIt();
                 u.updateRank(1);
                 v.updateRank(1); 
                 }
-                else { message = "The game is over"; }
+                else { message = "The game is over"; color = "green"; }
                 }
-
-            Base.close();
 
             Map map = new HashMap();
             map.put("game_id", game_id);
             map.put("message", message);
+            map.put("colored", color);
+            map.put("finished", finished);
+            map.put("p1id", id_player_1);
+            map.put("p2id", id_player_2);
 
             int k = 1;
             for (int i = 0; i < 6 ; i++) {
