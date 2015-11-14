f409ede80172eb7c1c7ad5cc7c23900ffab41ff7
ibongiovanni <ignaciobongiovanni@gmail.com>
2015-11-14 19:57:44 -0300

    Add new features to User model

--- src/main/java/com/unrc/app/User.java
+++ src/main/java/com/unrc/app/User.java
@@ -49,4 +49,22 @@ public class User extends Model {
   public String userName() {
     return this.getString("username"); 
   }
+
+  public int wins(){
+    return Math.toIntExact(Game.count("(winner = ?) and (player_1!=1 and player_2!=1)",this.getInteger("id")));
+  }
+
+  public int matches(){
+    int id = this.getInteger("id");
+    return Math.toIntExact(Game.count("(player_1=? or player_2=?) and (player_1!=1 and player_2!=1)", id, id));
+  }
+
+  public int draws(){
+    int id = this.getInteger("id");
+    return Math.toIntExact(Game.count("winner=0 and (player_1=? or player_2=?) and (player_1!=1 and player_2!=1)", id, id));
+  }
+
+  public int losses(){
+    return (matches() - wins() - draws());
+  }
 }
